61bc2cd9a5f38a361d5b4d88fc163188991f4d36
Slav Ivanyuk <slav.isv@gmail.com>
2015-01-25 17:36:51 -0500

    added script to automate build and nuget release

---
+++ HekadGateway/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[ assembly: AssemblyTitle( "Hekad Gateway" ) ]
+[ assembly: AssemblyDescription( "Assembly for pushing data to remote Hekad process." ) ]
+[ assembly: AssemblyConfiguration( "" ) ]
+[ assembly: AssemblyCompany( "Agile Harbor" ) ]
+[ assembly: AssemblyProduct( "HekadGateway" ) ]
+[ assembly: AssemblyCopyright( "Copyright © Agile Harbor" ) ]
+[ assembly: AssemblyTrademark( "" ) ]
+[ assembly: AssemblyCulture( "" ) ]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+
+[ assembly: ComVisible( false ) ]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[ assembly: Guid( "7496c9b9-c75d-43ad-b76a-ac3117a3b066" ) ]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+
+[ assembly: AssemblyVersion( "1.1.0.0" ) ]
